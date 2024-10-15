7f3efc956224f28647a6839e3b1dba691f47ecf4
Paulin-NIYOBYOSE <niyobyosepaulin20@gmail.com>
2024-10-15 23:14:36 +0200

    Styled projects section

--- src/components/MyServices.js
+++ src/components/MyServices.js
@@ -3,32 +3,32 @@ import { FaCode, FaPaintBrush, FaMobileAlt, FaTools, FaComments, FaClipboardList
 
 const services = [
     {
-        icon: <FaCode size={50} className="text-purple-600" />,
+        icon: <FaCode size={50} className="text-indigo-500" />,
         title: 'Web Design',
         description: 'I provide professional web design services, creating stunning and user-friendly websites tailored to your needs.'
     },
     {
-        icon: <FaPaintBrush size={50} className="text-purple-600" />,
+        icon: <FaPaintBrush size={50} className="text-indigo-500" />,
         title: 'Graphic Design',
         description: 'I offer graphic design services to craft visually appealing and impactful designs that elevate your brand identity '
     },
     {
-        icon: <FaMobileAlt size={50} className="text-purple-600" />,
+        icon: <FaMobileAlt size={50} className="text-indigo-500" />,
         title: 'App Design',
         description: 'I offer mobile development services, crafting intuitive and engaging applications for both iOS and Android. '
     },
     {
-        icon: <FaTools size={50} className="text-purple-600" />,
+        icon: <FaTools size={50} className="text-indigo-500" />,
         title: 'Web Development',
         description: 'I provide web development services, creating responsive and user-friendly websites.'
     },
     {
-        icon: <FaComments size={50} className="text-purple-600" />,
+        icon: <FaComments size={50} className="text-indigo-500" />,
         title: 'Consultancy and Support',
         description: 'I offer IT consultancy and support services to help optimize your technology infrastructure.'
     },
     {
-        icon: <FaClipboardList size={50} className="text-purple-600" />,
+        icon: <FaClipboardList size={50} className="text-indigo-500" />,
         title: 'Illustration Design',
         description: "I provide illustration design services to create unique and engaging visuals"
     },
@@ -38,17 +38,17 @@ const MyServices = () => {
     return (
         <section className="md:px-[80px] bg-white dark:bg-gray-800" id='service'>
             <div className="container mx-auto text-center px-4">
-                <h2 className="text-4xl font-bold text-gray-700 dark:text-white mb-8">My Services</h2>
+                <h2 className="text-4xl font-bold text-gray-800 dark:text-white mb-8">My Services</h2>
                 <p className="text-gray-600 dark:text-gray-400 mb-12">
                     I offer a range of services designed to meet your needs and help you succeed.
                 </p>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                     {services.map((service, index) => (
-                        <div key={index} className="bg-gray-100 dark:bg-gray-700 border  border-purple-300 dark:border-indigo-500  p-6 rounded-lg shadow-md transition-transform transform hover:scale-105 my-4">
+                        <div key={index} className="bg-gray-100 dark:bg-gray-800 border  border-purple-300 dark:border-indigo-500  p-6 rounded-lg shadow-md transition-transform transform hover:scale-105 my-4">
                             <div className="flex justify-center mb-4">
                                 {service.icon}
                             </div>
-                            <h3 className="text-xl font-semibold text-gray-700 dark:text-white mb-2">{service.title}</h3>
+                            <h3 className="text-xl font-semibold text-gray-800 dark:text-white mb-2">{service.title}</h3>
                             <p className="text-gray-600 dark:text-gray-400">{service.description}</p>
                         </div>
                     ))}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,30 +8,33 @@ const projects = [
         description: "A Next-Gen Netflix Clone with Enhanced Features",
         stack: ["React", "Tailwind CSS"],
         githubLink: "https://github.com/Paulin-NIYOBYOSE/Cineflix",
-        liveLink: "https://cineflix0-movies.vercel.app/"
+        liveLink: "https://cineflix0-movies.vercel.app/",
+        image: "./cineflix.png"
     },
     {
         title: "IS Technology",
         description: "An online tech hub for selling all electronic devices",
         stack: ["React", "Tailwind", "Java"],
         githubLink: "https://github.com/Paulin-NIYOBYOSE/Is-Tech",
-        liveLink: "https://is-tech-eta.vercel.app/"
+        liveLink: "https://is-tech-eta.vercel.app/",
+        image: "./istech.png"
     },
     {
         title: "FXMA",
         description: "An online platform for educating people on forex trading, my trading bot, and signals",
         stack: ["Next.js", "Python", "Java"],
         githubLink: "https://github.com/Paulin-NIYOBYOSE/FOREX-MINERS-ACADEMY",
-        liveLink: "https://github.com/Paulin-NIYOBYOSE/FOREX-MINERS-ACADEMY"
+        liveLink: "https://github.com/Paulin-NIYOBYOSE/FOREX-MINERS-ACADEMY",
+        image: "./fxma.png"
     },
     {
         title: "Joblift",
-        description: "This application allows users to track jobs they’ve applied for, monitor their status, set reminders for follow-ups, and manage their job search effectively.",
+        description: "This application allows connects job seekers and recruiters, helping people find jobs easily.",
         stack: ["Python", "Java", "Next.js"],
         githubLink: "https://github.com/Paulin-NIYOBYOSE/Is-Tech",
-        liveLink: "https://github.com/Paulin-NIYOBYOSE"
+        liveLink: "https://github.com/Paulin-NIYOBYOSE",
+        image: "./fxma.png"
     },
-
 ];
 
 const Projects = () => {
@@ -40,21 +43,30 @@ const Projects = () => {
             <h2 className="text-center text-3xl md:text-5xl font-bold mb-10 dark:text-white">Projects</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-5 md:px-20">
                 {projects.map((project, index) => (
-                    <div key={index} className="dark:bg-gray-800 bg-white p-5 rounded-lg flex flex-col justify-center items-center shadow-xl font-poppins">
-                        <h3 className="text-2xl font-bold mb-3 dark:text-white font-poppins">{project.title}</h3>
+                    <div key={index} className="relative group dark:bg-gray-800 bg-white p-5 rounded-lg shadow-xl font-poppins  border border-indigo-500">
+                        <div className="overflow-hidden rounded-lg lg:h-[120px]">
+                            <img
+                                src={project.image}
+                                alt={project.title}
+                                className="w-full h-36 rounded-md object-cover mb-5 transition-transform duration-300 transform group-hover:scale-110"
+                            />
+                            <div className="absolute inset-0 flex justify-center items-center rounded-xl bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="text-white mx-2 text-2xl">
+                                    <FaGithub />
+                                </a>
+                                <a href={project.liveLink} target="_blank" rel="noopener noreferrer" className="text-white mx-2 text-2xl">
+                                    <FaExternalLinkAlt />
+                                </a>
+                            </div>
+                        </div>
+                        <h3 className="text-2xl font-bold mb-3 dark:text-white">{project.title}</h3>
                         <p className="text-gray-400 mb-5">{project.description}</p>
                         <div className="flex space-x-3 mb-4">
                             {project.stack.map((tech, i) => (
-                                <span key={i} className="dark:bg-gray-700 bg-gray-600 border border-indigo-500 text-white px-2 py-1 rounded text-xs sm:text-sm">{tech}</span>
+                                <span key={i} className="dark:bg-gray-700 bg-gray-600 border border-indigo-500 text-white px-2 py-1 rounded text-xs sm:text-sm">
+                                    {tech}
+                                </span>
                             ))}
-                        </div>
-                        <div className="flex justify-between items-center gap-5">
-                            <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="text-xl dark:text-white">
-                                <FaGithub />
-                            </a>
-                            <a href={project.liveLink} target="_blank" rel="noopener noreferrer" className="text-xl dark:text-white">
-                                <FaExternalLinkAlt />
-                            </a>
                         </div>
                     </div>
                 ))}
